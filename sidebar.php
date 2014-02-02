9d5ce0ba3bb7fea97c97003cce62eddf329dc7cd
NickS <nskelsey@gmail.com>
2014-02-01 19:29:38 -0500

    yay

--- sidebar.php
+++ sidebar.php
@@ -5,7 +5,7 @@
    <div class="button"><a href="/splost/revenue/">Montly Revenue</a></div>
   </div>
   <div style="clear:both;"></div>
-   
+
   <h5>SPLOST CATEGORIES & FOCUS AREAS</h5>
     <div class="menu">
     <ul class="menu_cat">
@@ -27,7 +27,7 @@
    <li class="if first"><a href="/public-safety/bibb-storm-drainage/">Bibb Storm Drainage</a></li>
   </ul>
 </div><!-- #menu END -->
-     
+
 
 <div style="clear:both;"></div>
   <h5>PRESS & DOCUMENTS</h5>
@@ -36,7 +36,7 @@
   </div><!-- #menu END -->
 
   <div class="searcher">
- 	  <?php get_search_form(); ?>
- 	</div>
+    <?php get_search_form(); ?>
+  </div>
 </div><!-- #sidebar END -->
-   
+
